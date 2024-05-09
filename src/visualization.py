0a6046894c374e9d6006c127257115a509e5b172
Karl-Augustin Jahnel <freelance@karlaugustinjahnel.de>
2024-05-09 16:12:16 +0200

    added new data

--- src/visualization.py
+++ src/visualization.py
@@ -26,12 +26,12 @@ def visualize_tree(node, dot=None, parent_id=None, edge_label="", node_id=0):
     if dot is None:
         dot = Digraph()
         dot.attr('node', shape='box')  # Set the shape of nodes to boxes
-        initial_label = f'"{node_id}: {node.label if node.is_leaf_node() else node.condition}"'
+        initial_label = f'"{node.label if node.is_leaf_node() else node.condition}"'
         dot.node(str(node_id), initial_label)
         parent_id = node_id
 
     if node.is_leaf_node():
-        leaf_label = f'"Leaf {node_id}: {node.label}"'
+        leaf_label = f'"{node.label}"'
         dot.node(str(node_id), leaf_label)
         if parent_id is not None:
             dot.edge(str(parent_id), str(node_id), label=edge_label)
@@ -43,7 +43,7 @@ def visualize_tree(node, dot=None, parent_id=None, edge_label="", node_id=0):
             # For categorical features, the split is binary but visualized as one node
             condition_label = f"{node.condition.feature} == {node.condition.value}"
 
-        node_label = f'"{node_id}: {condition_label}"'
+        node_label = f'"{condition_label}"'
         dot.node(str(node_id), node_label)
         if parent_id is not None:
             dot.edge(str(parent_id), str(node_id), label=edge_label)
